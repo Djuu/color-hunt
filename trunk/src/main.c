eaf1b055ba87bd826edb24ace77482e1a96a6a20
cybermica@gmail.com <cybermica@gmail.com@bc57808b-bcce-6010-7f1a-7e64360ba157>
2012-10-23 09:29:16 +0000

    ha

---
+++ trunk/src/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <SDL/SDL.h>
+
+#include "SDLGame.h"
+
+int main(int argc, char *argv[])
+{
+
+
+	SdlGame pSdlGame;	
+	initSDL(&pSdlGame);
+	loopSDL(&pSdlGame);
+
+	return 0;
+}
