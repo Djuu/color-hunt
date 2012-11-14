4ecfc89a4af9d12dab4e139facaac6462372f002
baghinyan.areg@gmail.com <baghinyan.areg@gmail.com@bc57808b-bcce-6010-7f1a-7e64360ba157>
2012-11-14 18:12:00 +0000

    SDL_Rect

--- src/Physics.c
+++ src/Physics.c
@@ -38,14 +38,29 @@ float path (Character *pChar, Map *pMap)
 		
 			/*tab=realloc(tab,(j+1)*sizeof(char));	
 			tab[j]=getMapXY(pMap, (int)posX, (int)posY);*/
+			if (getMapXY(pMap, (int)(posX-0.5), (int)posY)=='#' || getMapXY(pMap, (int)(posXl-0.5), (int)posYl)=='#')
+			{
+				pChar -> air = 1;
+			}
+			else if (getMapXY(pMap, (int)(posXd+0.5), (int)posYd)=='#' || getMapXY(pMap, (int)(posXr+0.5), (int)posYr)=='#')
+			{
+				pChar -> air = 2;
+			}
+			else 
+			{
+				pChar -> air = 0;
+			}
 			if(getMapXY(pMap, (int)posX, (int)posY)=='#' || getMapXY(pMap, (int)posXd, (int)posYd)=='#' 
 			|| getMapXY(pMap, (int)posXr, (int)posYr)=='#' || getMapXY(pMap, (int)posXl, (int)posYl)=='#') 
 			{
 				
 				return i-0.01;
-				
 
 			}
+	
+			
+			
+			
 			setPosi (current, posX, posY,0);
 			
 		
@@ -56,14 +71,15 @@ float path (Character *pChar, Map *pMap)
 
 void collision (Character *pChar, Map *pMap)
 {
-
+	
 	pChar->cPosi.v_y*=path (pChar, pMap);
 	pChar->cPosi.v_x*=path (pChar, pMap);
 	
 	pChar->cPosi.y+=pChar->cPosi.v_y;
 	pChar->cPosi.x+=pChar->cPosi.v_x;
 	
-	if (getMapXY(pMap, (int)(pChar->cPosi.x), (int)(pChar->cPosi.y+1.5))=='#' || getMapXY(pMap, (int)(pChar->cPosi.x+1), (int)(pChar->cPosi.y+1.5))=='#')
+	if (getMapXY(pMap, (int)(pChar->cPosi.x), (int)(pChar->cPosi.y+1.5))=='#' || 
+	    getMapXY(pMap, (int)(pChar->cPosi.x+1), (int)(pChar->cPosi.y+1.5))=='#')
 	{
 		pChar->floor = 1;
 	} 
@@ -73,6 +89,6 @@ void collision (Character *pChar, Map *pMap)
 		
 	}
 	
- 
+
 	
 }
