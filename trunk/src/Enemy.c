333af249bb9e780193bccc9d0672c517737445af
baghinyan.areg@gmail.com <baghinyan.areg@gmail.com@bc57808b-bcce-6010-7f1a-7e64360ba157>
2012-11-20 07:17:32 +0000

    update

--- trunk/src/Enemy.c
+++ trunk/src/Enemy.c
@@ -1,1 +1,54 @@
 #include "Enemy.h"
+/*
+void initEnemy(Enemy *enemy, int num)
+{
+	assert(num >= 0);
+	enemy -> number = num;
+	int i = 0;
+	for (i = 0; i<enemy -> num; i++)
+	{
+		initChar (enemy -> eChar[i]);
+	}
+	
+	enemy -> id = 0;
+}
+
+
+Position getPosiEnemy (const Enemy *enemy)
+{
+	assert(enemy -> eChar != NULL);
+	return pChar -> cPosi;
+}
+
+void setPosiEnemy (Enemy *enemy, const float x, const float y, const float z)
+{
+	assert(pChar != NULL);
+	pChar -> cPosi.x = x;
+	pChar -> cPosi.y = y;
+	pChar -> cPosi.z = z;
+}
+
+int getLifeEnemy (const Enemy *enemy)
+{
+	assert(pChar -> life >= 0 || pChar -> life <= 100);
+	return pChar -> life;
+}
+
+void setLifeEnemy (Enemy *enemy, int nbLife)
+{
+	assert(nbLife >= 0 || nbLife <= 0);
+	pChar -> life = nbLife;
+}
+
+int getManaEnemy (const Enemy *enemy)
+{
+	assert(pChar -> mana >= 0 || pChar -> mana <= 100);
+	return pChar -> mana;
+}
+
+void setManaEnemy (Enemy *enemy, int nbMana)
+{
+	assert(nbMana >= 0 || nbMana <= 0);
+	pChar -> mana = nbMana;
+}
+*/
